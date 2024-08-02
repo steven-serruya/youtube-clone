7282782b218ffe017dadcdfc56e8158121877024
Steven <steven@Stevens-MacBook-Air.local>
2024-08-02 09:53:26 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as NavBar } from "./NavBar";
+export { default as Feed } from "./Feed";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as SearchBar } from "./SearchBar";
+export { default as Videos } from "./Videos";
+export { default as SideBar } from "./SideBar";
