de7babb31df79374aa86c699f554f750a1833520
Steven <steven@Stevens-MacBook-Air.local>
2024-08-06 14:58:57 -0500

    add Loader

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -27,30 +27,38 @@ const VideoDetail = () => {
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
-        <Box flex={1}>
-          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
-            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
-              {title}
-            </Typography>
-            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
-              <Link to={`/channel/${channelId}`}>
-                <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
-                  {channelTitle}
-                  <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
-                </Typography>
-              </Link>
-              <Stack direction="row" gap="20px" alignItems="center">
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
-                  {parseInt(viewCount).toLocaleString()} views
-                </Typography>
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
-                  {parseInt(likeCount).toLocaleString()} likes
-                </Typography>
-              </Stack>
-            </Stack>
-          </Box>
-        </Box>
+      <Box flex={1}>
+  <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+  <ReactPlayer
+  url={`https://www.youtube.com/watch?v=${id}`}
+  className="react-player"
+  width="100%"
+  height="950px"  // Set a specific height
+  controls
+/>
+
+    <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+      {title}
+    </Typography>
+    <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
+      <Link to={`/channel/${channelId}`}>
+        <Typography variant={{ sm: "subtitle1", md: 'h6' }} color="#fff" >
+          {channelTitle}
+          <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+        </Typography>
+      </Link>
+      <Stack direction="row" gap="20px" alignItems="center">
+        <Typography variant="body1" sx={{ opacity: 0.7 }}>
+          {parseInt(viewCount).toLocaleString()} views
+        </Typography>
+        <Typography variant="body1" sx={{ opacity: 0.7 }}>
+          {parseInt(likeCount).toLocaleString()} likes
+        </Typography>
+      </Stack>
+    </Stack>
+  </Box>
+</Box>
+
         <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
           <Videos videos={videos} direction="column" />
         </Box>
